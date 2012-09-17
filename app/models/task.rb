2b9991a69679ba9b0cb92aaf4a4a3b8febd604f3
Omar Shammas <omar.shammas@gmail.com>
2012-09-17 00:48:49 -0400

    Small changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
   attr_accessible :block, :button, :errors, :position, :time, :user_id
 
   belongs_to :user
+
+  scope :familiar, where(block: 'familiarization')
+  scope :performance, where(block: 'performance')
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :ip, :turk_id
 
-  has_many :task
+  has_many :tasks, dependent: :destroy
 
   #need to serialize the ip because the request can return a list of ip addresses some times
   serialize :ip
-
 end
